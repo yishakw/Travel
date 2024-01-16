eebf17ecf2db5b944fa97e6fda07cdc437e67742
Yishak Wondimgezahu <yishakwondimgezahu12@gmail.com>
2024-01-16 10:32:02 +0300

    starting files setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
